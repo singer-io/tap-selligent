0db0f65f4a2562937711cfd0e6ee4086868f147e
Andy Lu <andy@stitchdata.com>
2023-05-30 20:26:26 +0000

    Bump version, update changelog

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tap-selligent',
-    version='0.0.2',
+    version='0.0.3',
     description='Singer.io tap for extracting data from the Selligent API',
     author='Stitch',
     url='http://singer.io',
